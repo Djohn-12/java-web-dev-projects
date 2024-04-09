89e264f07ea6175363efb31e147922944eb5bd6b
Djohn-12 <djjohnson1101@gmail.com>
2024-04-08 20:11:45 -0500

    studio classes

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,9 @@
+package org.launchcode;
+
+public class Menu {
+    private String cheeseBurger = "Bacon Cheeseburger";
+    private String pasta = "Big Baked Lasagna";
+    private String desert = "Chocolate Lava Cake";
+    private String drinks = "Old Fashion";
+    private String
+}
